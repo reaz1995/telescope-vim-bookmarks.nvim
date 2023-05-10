c57f47db0a0eab8ad8c620a8a465680027a8c97d
reaz1995 <sebastian.langner00@gmail.com>
2023-05-10 10:25:29 +0200

    (fix) delete bookmarks actions

--- lua/telescope/_extensions/vim_bookmarks/actions.lua
+++ lua/telescope/_extensions/vim_bookmarks/actions.lua
@@ -3,9 +3,19 @@ local transform_mod = require('telescope.actions.mt').transform_mod
 local actions = require('telescope.actions')
 local action_state = require('telescope.actions.state')
 
-function delete_bookmark(entry)
+local function bookmark_save_file(file)
+    if vim.g.bookmark_manage_per_buffer == 1 then
+        return vim.fn['g:BMBufferFileLocation'](file) or vim.loop.cwd() .. '/.vim-bookmarks'
+    elseif vim.g.bookmark_save_per_working_dir == 1 then
+        return vim.fn['g:BMWorkDirFileLocation']() or vim.loop.cwd() .. '/.vim-bookmarks'
+    end
+    return vim.g.bookmark_auto_save_file
+end
+
+local function delete_bookmark(entry)
     vim.fn['bm_sign#del'](entry.filename, tonumber(entry.value.sign_idx))
     vim.fn['bm#del_bookmark_at_line'](entry.filename, tonumber(entry.lnum))
+    vim.fn['BookmarkSave'](bookmark_save_file(vim.g.bm_current_file), 1)
 end
 
 local delete_at_cursor = function(prompt_bufnr)
@@ -20,12 +30,10 @@ local delete_selected = function(prompt_bufnr)
         delete_bookmark(entry)
     end
 
-    -- Remove all multi selections. 
-    -- TODO There's probably an easier way to do this? 
-    --      Couldn't find any API for this
-    for row = 0, picker.max_results-1 do 
+    -- Remove all multi selections.
+    for row = 0, picker.max_results - 1 do
         local entry = picker.manager:get_entry(picker:get_index(row))
-        if entry then 
+        if entry then
             picker:remove_selection(row)
         end
     end
@@ -34,9 +42,7 @@ end
 
 return transform_mod {
     delete_at_cursor = delete_at_cursor,
-
     delete_selected = delete_selected,
-
     delete_selected_or_at_cursor = function(prompt_bufnr)
         if #action_state.get_current_picker(prompt_bufnr):get_multi_selection() > 0 then
             delete_selected(prompt_bufnr)
@@ -44,10 +50,7 @@ return transform_mod {
             delete_at_cursor(prompt_bufnr)
         end
     end,
-
     delete_all = function(prompt_bufnr)
         vim.cmd('BookmarkClearAll')
     end
 }
-
-
